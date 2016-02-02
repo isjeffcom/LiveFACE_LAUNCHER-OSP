5642659e0c3441add4b9ad2dd579261352e6c7c6
isjeffcom <abbcnnhktv@gmail.com>
2016-02-02 01:58:03 +0000

    Update Setting Logic and Option

--- app/src/main/java/cc/flydev/launcher/settings/InterfaceActivity.java
+++ app/src/main/java/cc/flydev/launcher/settings/InterfaceActivity.java
@@ -41,6 +41,7 @@ public class InterfaceActivity extends AbstractBarPreferenceActivity {
     class InterfaceFragment extends PreferenceFragment implements OnPreferenceChangeListener {
         private static final String KEY_ICONPACK_CHOOSER = "interface_iconpack_chooser";
         private static final String KEY_ICONPACK_CURRENT = "interface_iconpack_current";
+        private static final String ICON_SIZE_VALUE = "null";
 
         private Preference mCurrent;
         private Preference mChooser;
@@ -50,6 +51,7 @@ public class InterfaceActivity extends AbstractBarPreferenceActivity {
         private CheckBoxPreference mEnableDrawer;
         private ListPreference mHotseatIconSize;
         private CheckBoxPreference mAutoRotate;
+        private String mSizeValue;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -61,6 +63,8 @@ public class InterfaceActivity extends AbstractBarPreferenceActivity {
             mHomescreenIconSize = (ListPreference) findPreference(SettingsProvider.KEY_INTERFACE_HOMESCREEN_DRAWER_ICON_SIZE);
             mEnableDrawer = (CheckBoxPreference) findPreference(SettingsProvider.KEY_INTERFACE_HOMESCREEN_DRAWER_ENABLE_DRAWER);
             mHotseatIconSize = (ListPreference) findPreference(SettingsProvider.KEY_INTERFACE_HOTSEAT_ICON_SIZE);
+            mSizeValue = ICON_SIZE_VALUE;
+
 
 
             mHideIcon = findPreference("interface_hide_shortcut");
@@ -71,27 +75,39 @@ public class InterfaceActivity extends AbstractBarPreferenceActivity {
             try {
                 mCurrent.setSummary(getPackageManager().getPackageInfo(iconPack, 0).applicationInfo.loadLabel(getPackageManager()));
             } catch (Exception e) {
-                // So what?
             }
 
-            int globalFontSize = SettingsProvider.getInt(getActivity(), SettingsProvider.KEY_INTERFACE_GLOBAL_FONT_SIZE, 13);
+            /*int globalFontSize = SettingsProvider.getInt(getActivity(), SettingsProvider.KEY_INTERFACE_GLOBAL_FONT_SIZE, 13);
             mGlobalFontSize.setSummary(globalFontSize + " sp");
-            mGlobalFontSize.setText(String.valueOf(globalFontSize));
+            mGlobalFontSize.setText(String.valueOf(globalFontSize));*/
 
             int homescreenIconSize = SettingsProvider.getInt(getActivity(), SettingsProvider.KEY_INTERFACE_HOMESCREEN_DRAWER_ICON_SIZE, 48);
-            mHomescreenIconSize.setSummary(homescreenIconSize + " dp");
+            switch (homescreenIconSize){
+                case 48:
+                    mSizeValue = getString(R.string.iconSizeSmall).toString();
+                    break;
+
+                case 52:
+                    mSizeValue = getString(R.string.iconSizeMid).toString();;
+                    break;
+
+                case 58:
+                    mSizeValue = getString(R.string.iconSizeLarge).toString();;
+                    break;
+            }
+            mHomescreenIconSize.setSummary(mSizeValue);
             mHomescreenIconSize.setValue(String.valueOf(homescreenIconSize));
 
             int hotseatIconSize = SettingsProvider.getInt(getActivity(), SettingsProvider.KEY_INTERFACE_HOTSEAT_ICON_SIZE, 48);
-            mHotseatIconSize.setSummary(hotseatIconSize + " dp");
-            mHotseatIconSize.setValue(String.valueOf(hotseatIconSize));
+            //mHotseatIconSize.setSummary(hotseatIconSize + " dp");
+            //mHotseatIconSize.setValue(String.valueOf(hotseatIconSize));
 
             mEnableDrawer.setChecked(SettingsProvider.getBoolean(getActivity(), SettingsProvider.KEY_INTERFACE_HOMESCREEN_DRAWER_ENABLE_DRAWER, false));
 
-            mGlobalFontSize.setOnPreferenceChangeListener(this);
+            //mGlobalFontSize.setOnPreferenceChangeListener(this);
             mHomescreenIconSize.setOnPreferenceChangeListener(this);
             mEnableDrawer.setOnPreferenceChangeListener(this);
-            mHotseatIconSize.setOnPreferenceChangeListener(this);
+            //mHotseatIconSize.setOnPreferenceChangeListener(this);
             mAutoRotate = (CheckBoxPreference) findPreference(SettingsProvider.KEY_EFFECTS_GLOBAL_AUTO_ROTATE);
 
             // Initialize
@@ -122,14 +138,15 @@ public class InterfaceActivity extends AbstractBarPreferenceActivity {
             boolean needsRestart = false;
             if (preference == mGlobalFontSize) {
                 int size = newValue.equals("") ? 13 : Integer.parseInt((String) newValue);
-                mGlobalFontSize.setSummary(size + " sp");
+                //mGlobalFontSize.setSummary(size + " sp");
                 SettingsProvider.putInt(getActivity(), SettingsProvider.KEY_INTERFACE_GLOBAL_FONT_SIZE, size);
                 ret = true;
                 needsRestart = true;
             } else if (preference == mHomescreenIconSize) {
                 int size = newValue.equals("") ? 48 : Integer.parseInt((String) newValue);
-                mHomescreenIconSize.setSummary(size + " dp");
                 SettingsProvider.putInt(getActivity(), SettingsProvider.KEY_INTERFACE_HOMESCREEN_DRAWER_ICON_SIZE, size);
+                SettingsProvider.putInt(getActivity(), SettingsProvider.KEY_INTERFACE_HOTSEAT_ICON_SIZE, size);
+                mHomescreenIconSize.setSummary(mSizeValue);
                 ret = true;
                 needsRestart = true;
             } else if (preference == mHotseatIconSize) {
